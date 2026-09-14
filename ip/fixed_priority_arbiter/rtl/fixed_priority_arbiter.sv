39866e597bb76b8b3094c7e11935369173f021b0
Justin Turcotte <jjturcot@gmail.com>
2026-09-14 12:59:32 -0400

    Add assertions to fixed arbiter

--- ip/fixed_priority_arbiter/rtl/fixed_priority_arbiter.sv
+++ ip/fixed_priority_arbiter/rtl/fixed_priority_arbiter.sv
@@ -4,7 +4,24 @@ module fixed_priority_arbiter #(
     input  logic [N-1:0] req_i,
     output logic [N-1:0] gnt_o
 );
+  `include "formal_macros.svh"
+  `ASSERT_INIT(CheckNGreaterZero_A, N > 0)
+
   // isolates lowest set bit
   assign gnt_o = req_i & (-req_i);
 
+  // Assertions
+  `ASSERT_KNOWN(GrantKnown_A, gnt_o)
+  `ASSERT(HotOne_A, $onehot0(gnt_o))
+
+  generate
+    for (genvar i = 0; i < N; i++) begin : gen_priority_checks
+      `ASSERT(GrantNeedsReq_A, gnt_o[i] |-> req_i[i])
+
+      if (i > 0) begin : gen_check_priority
+        `ASSERT(StrictPriority_A, gnt_o[i] |-> (req_i[i-1:0] == '0))
+      end
+
+    end
+  endgenerate
 endmodule
